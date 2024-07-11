4ef5a54c632c79e8166c12b860ae651d453ba63a
Mio <mio.kokosson@gmail.com>
2024-07-11 15:12:27 +0200

    Add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Title from './components/Title'
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Title />
     </>
   )
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+
+function Title() {
+
+  return (
+    <>
+      Star Wars Universe
+    </>
+  )
+}
+
+export default Title
