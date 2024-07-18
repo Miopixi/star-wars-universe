b640f3c024fcb8b54b415653406e221ce5767a37
Mio <mio.kokosson@gmail.com>
2024-07-18 14:09:31 +0200

    Add footer with signature

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+
+import './components.css'
+
+function Footer() {
+
+  return (
+    <>
+      <footer className='signature red_glow'>
+        by Mio MMXXIV
+      </footer>
+    </>
+  )
+}
+
+export default Footer
